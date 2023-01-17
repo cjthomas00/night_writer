5436ed37c48a6870a920ffd68649d0dfd94b869e
Caleb Thomas <cjthomas00@comcast.net>
2023-01-17 15:29:58 -0700

    refactor #convert_to_english to be able to take multiple lines.

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -22,15 +22,23 @@ class NightReader
 
   def convert_to_english
     translator = Translator.new
-    braille_in_lines = File.readlines(@message_file)
-    convert_to_column = braille_in_lines.map do |line|
-      line.scan(/.{2}/)    # <== creates array with 2 character substrings ["O."] from braille_in_lines.
-    end
-    braille_letters = convert_to_column[0].zip(convert_to_column[1],convert_to_column[2])  # <== .zip combines the braille letter into 3 element array that will represent 1 braille letter.
+    multi_line_braille = Hash.new {|k,v| k[v] = [] }
+    braille_in_lines = File.readlines(@message_file).each_slice(3).to_a
+    braille_in_lines.each do |lines|
+       multi_line_braille[1] << lines[0].delete("\n")
+       multi_line_braille[2] << lines[1].delete("\n")  
+       multi_line_braille[3] << lines[2].delete("\n")  
+      end  
+    single_line = [multi_line_braille[1].join, multi_line_braille[2].join, multi_line_braille[3].join]
+
+    columns = single_line.map do |line|
+      line.scan(/.{2}/)    # <== creates array with 2 character substrings ["O."] from braille_in_lines, and makes them columns instead of rows.
+      end
+    braille_letters = columns[0].zip(columns[1],columns[2]) #<== .zip combines the braille letter into 3 element array that will represent 1 braille letter.
     english_letter = braille_letters.map do |letter|
       translator.braille_to_english_alphabet[letter]  # <== goes over the braille to english alphabet and turns braille into english word. 
-    end
-    english_letter.join # <== combines ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"] into "hello world"
+      end 
+    english_letter.join  #<== combines ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"] into "hello world"
   end
 
   def print_english_to_file(english)
